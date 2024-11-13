47653927bb11a91de28f232213867515e228bb60
Gabriele <146710176+gabriele-bassi@users.noreply.github.com>
2024-11-13 11:23:16 +0100

    anagrafica

---
+++ pages/anagrafica/app.js
@@ -0,0 +1,22 @@
+const inps = document.getElementsByClassName("campi");
+const tab = document.getElementById("table")
+let data = [];
+const caricaInfo = () => {
+    let row = []
+    for (let i = 0; i < inps.length; i++) {
+        row.push(inps[i].value);
+        inps[i].value = null;
+    }
+    data.push(row)
+    visualizza()
+}
+const visualizza = () => {
+    let l = data.length - 1;
+    for (let i = 0; i < data[l].length; i++) {
+        console.log(l)
+        let para = document.createElement("p")
+        para.className = "info"
+        para.innerText = data[l][i];
+        tab.appendChild(para);
+    }
+}
